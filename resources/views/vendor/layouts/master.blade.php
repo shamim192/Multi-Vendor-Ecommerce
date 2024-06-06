39a0ce8d35c7bf878b45f352be77dce50b652c6b
Md. Shamim Hossain <shamim.cse.gub@gmail.com>
2024-06-06 21:28:28 +0600

    wip:vendor crud

--- resources/views/vendor/layouts/master.blade.php
+++ resources/views/vendor/layouts/master.blade.php
@@ -5,13 +5,15 @@
   <meta charset="UTF-8">
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-  <title>One Shop || e-Commerce HTML Template</title>
-  <link rel="icon" type="image/png" href="{{asset('frontend/images/favicon.png')}}">
+  <title>
+    @yield('title')
+  </title>
+  <link rel="icon" type="image/png" href="images/favicon.png">
   <link rel="stylesheet" href="{{asset('frontend/css/all.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/select2.min.css')}}">
-  <link rel="stylesheet" href="{{ asset('frontend/css/summernote/summernote-bs4.css') }}">
   <link rel="stylesheet" href="{{asset('frontend/css/slick.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/jquery.nice-number.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/jquery.calendar.css')}}">
@@ -22,6 +24,10 @@
   <link rel="stylesheet" href="{{asset('frontend/css/ranger_style.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/jquery.classycountdown.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/venobox.min.css')}}">
+  <link rel="stylesheet" href="{{asset('backend/assets/modules/summernote/summernote-bs4.css')}}">
+
+  <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.css')}}">
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
   <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
@@ -37,8 +43,8 @@
   ==============================-->
   <div class="wsus__dashboard_menu">
     <div class="wsusd__dashboard_user">
-      <img src="{{asset('frontend/images/dashboard_user.jpg')}}" alt="img" class="img-fluid">
-      <p>anik roy</p>
+      <img src="{{asset(auth()->user()->image)}}" alt="img" class="img-fluid">
+      <p>{{auth()->user()->name}}</p>
     </div>
   </div>
   <!--=============================
@@ -49,9 +55,7 @@
   <!--=============================
     DASHBOARD START
   ==============================-->
-
-  @yield('content')
-
+    @yield('content')
   <!--=============================
     DASHBOARD START
   ==============================-->
@@ -102,24 +106,100 @@
   <script src="{{asset('frontend/js/venobox.min.js')}}"></script>
   <!--classycountdown js-->
   <script src="{{asset('frontend/js/jquery.classycountdown.js')}}"></script>
+
+  <script src="{{asset('backend/assets/modules/summernote/summernote-bs4.js')}}"></script>
+  <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+  <script src="{{asset('backend/assets/modules/moment.min.js')}}"></script>
+  <script src="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-  <script src="{{ asset('frontend/js/summernote/summernote-bs4.js') }}"></script>
+
   <!--main/custom js-->
   <script src="{{asset('frontend/js/main.js')}}"></script>
-  
+
   <script>
     @if ($errors->any())
         @foreach ($errors->all() as $error)
-             toastr.error("{{$error}}")
+            toastr.error("{{$error}}")
         @endforeach
     @endif
-</script>
+  </script>
 
-<script>
-$('.summernote').summernote({
-  height: 200,
-});
-</script>
+  <script>
+    /** summernote **/
+    $('.summernote').summernote({
+        height:150
+    })
+
+    /** date picker **/
+    $('.datepicker').daterangepicker({
+        locale: {
+            format: 'YYYY-MM-DD'
+        },
+        singleDatePicker: true
+    });
+  </script>
+
+    <!-- Dynamic Delete alart -->
+
+    <script>
+        $(document).ready(function(){
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+
+            $('body').on('click', '.delete-item', function(event){
+                event.preventDefault();
+
+                let deleteUrl = $(this).attr('href');
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        $.ajax({
+                            type: 'DELETE',
+                            url: deleteUrl,
+
+                            success: function(data){
+
+                                if(data.status == 'success'){
+                                    Swal.fire(
+                                        'Deleted!',
+                                        data.message,
+                                        'success'
+                                    )
+                                    window.location.reload();
+                                }else if (data.status == 'error'){
+                                    Swal.fire(
+                                        'Cant Delete',
+                                        data.message,
+                                        'error'
+                                    )
+                                }
+                            },
+                            error: function(xhr, status, error){
+                                console.log(error);
+                            }
+                        })
+                    }
+                })
+            })
+
+        })
+    </script>
+
+  @stack('scripts')
 </body>
 
 </html>
--- routes/vendor.php
+++ routes/vendor.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\VendorProfileController;
 use App\Http\Controllers\Backend\VendorShopProfileController;
+use App\Http\Controllers\Backend\VendorProductVariantController;
+use App\Http\Controllers\Backend\VendorProductVariantItemController;
+use App\Http\Controllers\Backend\VendorProductImageGalleryController;
 
 Route::get('dashboard', [VendorController::class,'dashboard'])->name('dashboard');
 Route::get('profile',[VendorProfileController::class,'index'])->name('profile');
@@ -11,3 +15,31 @@ Route::put('profile',[VendorProfileController::class,'updateProfile'])->name('pr
 Route::post('profile',[VendorProfileController::class,'updatePassword'])->name('profile.update.password');
 
 Route::resource('shop-profile', VendorShopProfileController::class);
+
+/** Product Routes */
+Route::get('product/get-subcategories', [VendorProductController::class, 'getSubCategories'])->name('product.get-subcategories');
+Route::get('product/get-child-categories', [VendorProductController::class, 'getChildCategories'])->name('product.get-child-categories');
+Route::put('product/change-status', [VendorProductController::class, 'changeStatus'])->name('product.change-status');
+Route::resource('products', VendorProductController::class);
+
+/** Products image gallery route */
+Route::resource('products-image-gallery', VendorProductImageGalleryController::class);
+
+/** Products variant route */
+Route::put('products-variant/change-status', [VendorProductVariantController::class, 'changeStatus'])->name('products-variant.change-status');
+Route::resource('products-variant', VendorProductVariantController::class);
+
+/** Products variant item route */
+Route::get('products-variant-item/{productId}/{variantId}', [VendorProductVariantItemController::class, 'index'])->name('products-variant-item.index');
+
+Route::get('products-variant-item/create/{productId}/{variantId}', [VendorProductVariantItemController::class, 'create'])->name('products-variant-item.create');
+
+Route::post('products-variant-item', [VendorProductVariantItemController::class, 'store'])->name('products-variant-item.store');
+
+Route::get('products-variant-item-edit/{variantItemId}', [VendorProductVariantItemController::class, 'edit'])->name('products-variant-item.edit');
+
+Route::put('products-variant-item-update/{variantItemId}', [VendorProductVariantItemController::class, 'update'])->name('products-variant-item.update');
+
+Route::delete('products-variant-item/{variantItemId}', [VendorProductVariantItemController::class, 'destroy'])->name('products-variant-item.destroy');
+
+Route::put('products-variant-item-status', [VendorProductVariantItemController::class, 'chageStatus'])->name('products-variant-item.chages-status');
